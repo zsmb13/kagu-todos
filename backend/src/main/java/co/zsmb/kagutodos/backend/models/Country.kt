3e2df569940e29d794f6b58d150f93f0d1a1638b
zsmb13 <braunmarci@gmail.com>
2017-10-27 19:39:34 +0200

    Removed unused model classes

--- backend/src/main/java/co/zsmb/kagutodos/backend/models/Country.kt
+++
@@ -1,3 +0,0 @@
-package co.zsmb.kagutodos.backend.models
-
-data class Country(val name: String, val code: String)
--- backend/src/main/java/co/zsmb/kagutodos/backend/models/Island.kt
+++
@@ -1,3 +0,0 @@
-package co.zsmb.kagutodos.backend.models
-
-data class Island(val name: String, val country: Country)
